b61ef6e650fd75c46ff8bd33daa2a70f523a41b7
222glongo <62153207+222glongo@users.noreply.github.com>
2021-10-26 10:41:33 -0400

    Add files via upload

---
+++ atomjs/sandbox.js
@@ -0,0 +1,101 @@
+/* <script>
+  alert('hello, world');
+</script> */
+
+// console.log(1);
+// console.log(2);
+
+/* let year= 2019;
+//log things to console
+console.log(age, year);
+
+age = 30;
+console.log(age);
+
+const points = 100;
+points = 50;
+
+var score = 75;
+console.log(score); */
+
+//strings
+/* console.log('hello world');
+let email = 'mario@thenetninja.co.uk'
+console.log(email);
+
+//sting concatention
+let firstname = 'Brandon';
+let lastname= 'Sanderson';
+
+let full name = firstname + ' ' + lastname;
+console.log(fullName);
+
+//getting characters
+console.log(fullName[2]);
+
+//string length
+console.log(fullName/length);
+
+//string method
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase());
+console.log(result, fullName);
+
+let index = email.indexOf('@'); */
+
+//common string methods
+/* let email= 'mario@thenetninja.co.uk';
+
+let result= email.lastIndexOf('n');
+
+let result = email.slice(0,10);
+
+let result = email.slice(2,5);
+
+let result = email.substr(4,10);
+
+let result = email.replace('n', 'w');
+
+console.log(result); */
+
+/* let radius=10;
+const pi=3.14;
+
+//console.log(radius, pi);
+
+math operators, +, -, *, /, **, %
+console.log(10/2);
+let result = radius % 3
+
+//order of operations - B I D M A switch
+
+
+let likes = 10;
+
+likes = likes + 1;
+likes**
+likes--;
+
+likes += 10;
+likes -= 5;
+likes *= 2;
+likes /= 2;
+
+//console.log(likes);
+
+//NaN - not a number
+console.log(5 / 'hello')
+
+let result = 'the blog has' + likes + 'likes';
+console.log(result);  */
+
+//template strings
+const title = 'Best read of 2019';
+const author = 'Mario';
+const likes = 30;
+
+//concatention way
+
+//template string way
+
+//creating html template
